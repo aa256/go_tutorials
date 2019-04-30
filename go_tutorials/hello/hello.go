3cc85491fe09ac49caec5e6e3006fd2b1d35c15d
Andy Arnold <andrewarnold@google.com>
2019-04-30 11:00:39 -0700

    Let's have fun with go.

---
+++ go_tutorials/hello/hello.go
@@ -0,0 +1,11 @@
+// https://golang.org/doc/code.html
+package main
+
+import (
+	"fmt"
+	"github.com/go_tutorials/stringutil"
+)
+
+func main() {
+	fmt.Println(stringutil.Reverse("Hello, world."))
+}
